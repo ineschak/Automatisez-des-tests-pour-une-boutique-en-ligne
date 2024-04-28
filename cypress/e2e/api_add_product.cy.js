834c6d2c42219cb5ec251e8a9e384be112b8e08c
ineschak <ineschakrouwn@gmail.com>
2024-04-28 16:37:50 +0200

    Correction test add product

--- cypress/e2e/api_add_product.cy.js
+++ cypress/e2e/api_add_product.cy.js
@@ -16,8 +16,9 @@ describe("Ajouter un produit dans le panier", () => {
       authToken = loginResponse.body.token; // Stocker le jeton d'authentification dans authToken
     });
   });
-  it("Ajouter un produit au panier", () => {
+  it("Ajouter un produit disponible au panier", () => {
     const productId = 5;
+
     // Récupérer le produit et vérifier s'il est disponible
     cy.request({
       method: "GET",
@@ -27,76 +28,94 @@ describe("Ajouter un produit dans le panier", () => {
       },
       failOnStatusCode: false,
     }).then((productResponse) => {
-      if (productResponse.status === 200) {
-        // Le produit est disponible, ajoutez-le au panier
-        cy.request({
-          method: "POST",
-          url: "http://localhost:8081/orders/add",
-          headers: {
-            Authorization: `Bearer ${authToken}`,
-          },
-          body: {
-            product: productId,
-            quantity: 1,
-          },
-        }).then((addToCartResponse) => {
-          // Vérifiez que le produit a été ajouté au panier avec succès
-          expect(addToCartResponse.status).to.eq(200);
-          // Vérifier le schéma de la réponse
-          expect(addToCartResponse.body)
-            .to.have.property("id")
-            .that.is.a("number");
-          expect(addToCartResponse.body)
-            .to.have.property("firstname")
-            .that.is.a("string");
-          expect(addToCartResponse.body)
-            .to.have.property("lastname")
-            .that.is.a("string");
-          expect(addToCartResponse.body)
-            .to.have.property("address")
-            .that.is.a("string");
-          expect(addToCartResponse.body)
-            .to.have.property("zipCode")
-            .that.is.a("string");
-          expect(addToCartResponse.body)
-            .to.have.property("city")
-            .that.is.a("string");
-          expect(addToCartResponse.body)
-            .to.have.property("date")
-            .that.is.a("string");
-          expect(addToCartResponse.body)
-            .to.have.property("validated")
-            .that.is.a("boolean");
-          expect(addToCartResponse.body)
-            .to.have.property("orderLines")
-            .that.is.an("array");
+      expect(productResponse.status).to.eq(200);
+      // Le produit est disponible, ajoutez-le au panier
+      cy.request({
+        method: "POST",
+        url: "http://localhost:8081/orders/add",
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+        body: {
+          product: productId,
+          quantity: 1,
+        },
+      }).then((addToCartResponse) => {
+        // Vérifiez que le produit a été ajouté au panier avec succès
+        expect(addToCartResponse.status).to.eq(200);
+        // Vérifier le schéma de la réponse
+        expect(addToCartResponse.body)
+          .to.have.property("id")
+          .that.is.a("number");
+        expect(addToCartResponse.body)
+          .to.have.property("firstname")
+          .that.is.a("string");
+        expect(addToCartResponse.body)
+          .to.have.property("lastname")
+          .that.is.a("string");
+        expect(addToCartResponse.body)
+          .to.have.property("address")
+          .that.is.a("string");
+        expect(addToCartResponse.body)
+          .to.have.property("zipCode")
+          .that.is.a("string");
+        expect(addToCartResponse.body)
+          .to.have.property("city")
+          .that.is.a("string");
+        expect(addToCartResponse.body)
+          .to.have.property("date")
+          .that.is.a("string");
+        expect(addToCartResponse.body)
+          .to.have.property("validated")
+          .that.is.a("boolean");
+        expect(addToCartResponse.body)
+          .to.have.property("orderLines")
+          .that.is.an("array");
 
-          // Vérifier le schéma des éléments de orderLines
-          const orderLine = addToCartResponse.body.orderLines[0];
-          expect(orderLine).to.have.property("id").that.is.a("number");
-          expect(orderLine).to.have.property("product").that.is.an("object");
-          expect(orderLine.product).to.have.property("id").that.is.a("number");
-          expect(orderLine.product)
-            .to.have.property("name")
-            .that.is.a("string");
-          expect(orderLine.product)
-            .to.have.property("description")
-            .that.is.a("string");
-          expect(orderLine.product)
-            .to.have.property("price")
-            .that.is.a("number");
-          expect(orderLine.product)
-            .to.have.property("picture")
-            .that.is.a("string");
-          const orderLine1 = addToCartResponse.body.orderLines[1];
-          expect(orderLine1).to.have.property("quantity");
-        });
-      } else {
-        // Le produit n'est pas disponible, retournez une erreur
-        throw new Error(
-          `Le produit avec l'ID ${productId} n'est pas disponible.`
-        );
-      }
+        // Vérifier le schéma des éléments de orderLines
+        const orderLine = addToCartResponse.body.orderLines[0];
+        expect(orderLine).to.have.property("id").that.is.a("number");
+        expect(orderLine).to.have.property("product").that.is.an("object");
+        expect(orderLine.product).to.have.property("id").that.is.a("number");
+        expect(orderLine.product).to.have.property("name").that.is.a("string");
+        expect(orderLine.product)
+          .to.have.property("description")
+          .that.is.a("string");
+        expect(orderLine.product).to.have.property("price").that.is.a("number");
+        expect(orderLine.product)
+          .to.have.property("picture")
+          .that.is.a("string");
+        const orderLine1 = addToCartResponse.body.orderLines[1];
+        expect(orderLine1).to.have.property("quantity");
+      });
+    });
+  });
+
+  it("Ajouter un produit non disponible dans le panier", () => {
+    const productId = 2;
+    // Récupérer le produit non disponible et vérifier la réponse
+    cy.request({
+      method: "GET",
+      url: `http://localhost:8081/products/${productId}`,
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+      failOnStatusCode: false,
+    }).then((productResponse) => {
+      expect(productResponse.status).to.eq(404);
+      cy.request({
+        method: "POST",
+        url: "http://localhost:8081/orders/add",
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+        body: {
+          product: productId,
+          quantity: 1,
+        },
+      }).then((addToCartResponse) => {
+        expect(addToCartResponse.status).to.eq(400);
+      });
     });
   });
 });
